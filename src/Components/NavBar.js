d416dcdca99955983b87a7f7ee3a64c8f336dcd8
aniket0332 <aniketlyadav0303@gmail.com>
2023-04-06 12:44:41 +0530

    first commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav class="nav-container flex-container">
+    <div class="logo">RecycleMate</div>
+    <div class="page-links-container flex-container">
+      <div class="nav-items">home</div>
+      <div class="nav-items">about</div>
+      <div class="nav-items">news</div>
+      <div class="nav-items">contact</div>
+      <div class="nav-items">sign up</div>
+    </div>
+  </nav>
+    </div>
+  )
+}
+
+export default NavBar;
